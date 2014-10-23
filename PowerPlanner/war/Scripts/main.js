5e1acc8380c59fa1aeae2602c6ecd1262b56e7b0
lamj1234 <jonathanlam123@gmail.com>
2014-10-23 04:40:04 -0700

    Added search place on intro page and on app page on the map Got rid of the expand/hide button on the app page. Now whole screen displays the map Hide all the resource boxes in the left toggle panel for now

--- PowerPlanner/war/Scripts/main.js
+++ PowerPlanner/war/Scripts/main.js
@@ -1,40 +1,31 @@
 $(document).ready(function() {
-	$(".expandSettingButton").on('click', function() {
-		$(".resourceBox").toggleClass('hide-resourcesBox');
-		$(".mapSettings").toggleClass('hide-resourcesBox');
-		$(".mapSelect").toggleClass('hide-resourcesBox');
-		$(".propertyButton").toggleClass('hide-resourcesBox');
-		$(".gMap").toggleClass('expandMap');
-		$(".settingsWidgets").toggleClass('hide-description');
-		$(".expandSettingButton").toggleClass('ui-icon-arrow-r');
-	});
 	resizeDiv();
+	$("a.start-button").click(startButtonClick);
 
-	
 	$("#showCheckboxWind").click(function() {
 		toggleHeatmapData(
 				$(this).is(':checked'),
 				$("#showCheckboxSolar").is(':checked'),
 				$("#showCheckboxHydro").is(':checked')
-				);
+		);
 	});
-	
+
 	$("#showCheckboxSolar").click(function() {
 		toggleHeatmapData(
 				$("#showCheckboxWind").is(':checked'),
 				$(this).is(':checked'),
 				$("#showCheckboxHydro").is(':checked')
-				);
+		);
 	});
-		
+
 	$("#showCheckboxHydro").click(function() {
 		toggleHeatmapData(
 				$("#showCheckboxWind").is(':checked'),
 				$("#showCheckboxSolar").is(':checked'),
 				$(this).is(':checked')
-				);
+		);
 	});
-	
+
 });
 
 window.onresize = function(event) {
@@ -48,8 +39,10 @@ function resizeDiv() {
 	$(".gMap").css({
 		'height' : vph + 'px'
 	});
-	$(".expandSettingButton").css({
-		'height' : vph + 'px'
-	});
+}
+function startButtonClick() {
+	if($("#pac-input-intro").val() != "")	{
+		$("#pac-input").val($("#pac-input-intro").val());
+	}
 }
 
--- PowerPlanner/war/Scripts/map_helper.js
+++ PowerPlanner/war/Scripts/map_helper.js
@@ -5,12 +5,105 @@ var wind_data = []; /* The wind data for the current heatmap view */
 var solar_data = []; /* The solar data for the current heatmap view */
 var hydro_data = []; /* The hydro data for the current heatmap view */
 
+//This example adds a search box to a map, using the Google Place Autocomplete
+//feature. People can enter geographical searches. The search box will return a
+//pick list containing a mix of places and predicted search terms.
+
+function initialize() {
+
+	var markers = [];
+	var map = new google.maps.Map(document.getElementById('googleMap'), {
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		zoom : 14,
+		maxZoom : 15,
+		minZoom : 8,
+		streetViewControl : false,
+		scaleControl : true,
+		center : new google.maps.LatLng(48.4647, -123.3085),
+		styles : [ {
+			featureType : "poi",
+			elementType : "labels",
+			stylers : [ {
+				visibility : "off"
+			} ]
+		} ]
+	});
+
+	var defaultBounds = new google.maps.LatLngBounds(
+			new google.maps.LatLng(48.4647, -123.3085),
+			new google.maps.LatLng(48.4647, -123.3085));
+	map.fitBounds(defaultBounds);
+
+//	Create the search box and link it to the UI element.
+	var input = /** @type {HTMLInputElement} */(
+			document.getElementById('pac-input'));
+	var inputIntro = /** @type {HTMLInputElement} */(
+			document.getElementById('pac-input-intro'));
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	var searchBox = new google.maps.places.SearchBox(
+			/** @type {HTMLInputElement} */(input));
+	var searchBoxIntro = new google.maps.places.SearchBox(
+			/** @type {HTMLInputElement} */(inputIntro));
+
+//	[START region_getplaces]
+//	Listen for the event fired when the user selects an item from the
+//	pick list. Retrieve the matching places for that item.
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+		for (var i = 0, marker; marker = markers[i]; i++) {
+			marker.setMap(null);
+		}
+
+		// For each place, get the icon, place name, and location.
+		markers = [];
+		var bounds = new google.maps.LatLngBounds();
+		for (var i = 0, place; place = places[i]; i++) {
+			var image = {
+					url: place.icon,
+					size: new google.maps.Size(71, 71),
+					origin: new google.maps.Point(0, 0),
+					anchor: new google.maps.Point(17, 34),
+					scaledSize: new google.maps.Size(25, 25)
+			};
+
+			// Create a marker for each place.
+			var marker = new google.maps.Marker({
+				map: map,
+				icon: image,
+				title: place.name,
+				position: place.geometry.location
+			});
+
+			markers.push(marker);
+
+			bounds.extend(place.geometry.location);
+		}
+
+		map.fitBounds(bounds);
+	});
+//	[END region_getplaces]
+
+//	Bias the SearchBox results towards places that are within the bounds of the
+//	current map's viewport.
+	google.maps.event.addListener(map, 'bounds_changed', function() {
+		var bounds = map.getBounds();
+		searchBox.setBounds(bounds);
+	});
+
+	return map;
+}
+
 /*
  * Initializes a test version of the map and heatmap, loading fake data on the
  * client side.
  */
 function mapTest() {
-	g_map = initMap();
+	g_map = initialize();
 	g_heatmap = initHeatmap(g_map);
 
 	attachHeatmap(g_heatmap, g_map);
@@ -32,7 +125,7 @@ function mapTest() {
  * Initializes the map, heatmap, and important event listeners.
  */
 function mapLoader() {
-	g_map = initMap();
+	g_map = initialize();
 	g_heatmap = initHeatmap(g_map);
 
 	attachHeatmap(g_heatmap, g_map);
@@ -96,8 +189,8 @@ function loadHeatmapData(map) {
  */
 function _eventHeatmapDataToggler() {
 	toggleHeatmapData($("#showCheckboxWind").is(':checked'), $(
-			"#showCheckboxSolar").is(':checked'), $("#showCheckboxHydro").is(
-			':checked'));
+	"#showCheckboxSolar").is(':checked'), $("#showCheckboxHydro").is(
+	':checked'));
 }
 
 /*
@@ -167,32 +260,6 @@ function _getHeatmapData(type, neLat, neLng, swLat, swLng) {
 }
 
 /*
- * Initializes map with current basic settings.
- */
-function initMap() {
-	var mapCanvas = document.getElementById('googleMap');
-	var mapOptions = {
-		zoom : 14,
-		maxZoom : 17,
-		minZoom : 8,
-		streetViewControl : false,
-		scaleControl : true,
-		center : new google.maps.LatLng(48.4647, -123.3085),
-		mapTypeId : google.maps.MapTypeId.ROADMAP,
-		styles : [ {
-			featureType : "poi",
-			elementType : "labels",
-			stylers : [ {
-				visibility : "off"
-			} ]
-		} ]
-	};
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-
-	return map;
-}
-
-/*
  * Initializes heatmap with current basic settings.
  */
 function initHeatmap(map) {
@@ -203,9 +270,9 @@ function initHeatmap(map) {
 		dissipating : false,
 		opacity : 0.4,
 		gradient : [ 'rgba(0,0,0,0)', 'rgba(255,0,0,1)', 'rgba(255,63,0,1)',
-				'rgba(255,127,0,1)', 'rgba(255,191,0,1)', 'rgba(255,255,0,1)',
-				'rgba(223,255,0,1)', 'rgba(191,255,0,1)', 'rgba(159,255,0,1)',
-				'rgba(127,255,0,1)', 'rgba(63,255,0,1)', 'rgba(0,255,0,1)' ]
+		             'rgba(255,127,0,1)', 'rgba(255,191,0,1)', 'rgba(255,255,0,1)',
+		             'rgba(223,255,0,1)', 'rgba(191,255,0,1)', 'rgba(159,255,0,1)',
+		             'rgba(127,255,0,1)', 'rgba(63,255,0,1)', 'rgba(0,255,0,1)' ]
 	});
 
 	return heatmap;
