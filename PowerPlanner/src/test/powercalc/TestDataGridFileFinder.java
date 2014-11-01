a69bac469e6fa2be11f622e9065ecb5fe58900bf
Prashant Chhabra <prashant.chhabra89@gmail.com>
2014-11-01 01:08:50 -0700

    Added java class to send path of json files along with test class

---
+++ PowerPlanner/src/test/powercalc/TestDataGridFileFinder.java
@@ -0,0 +1,15 @@
+package test.powercalc;
+import com.powerplanner.*;
+
+public class TestDataGridFileFinder {
+	String arr[];
+
+    public static void main(String[] args) {
+       DatabaseFileFInder dataGridFileFinder = new DatabaseFileFInder();
+    dataGridFileFinder.windFileFinder(48.463083,123.310996,48.463083,123.310996,"anu");
+  dataGridFileFinder.displayWindReturnarr();
+    dataGridFileFinder.solarFileFinder("Apr");
+    dataGridFileFinder.displaySolarFileList();
+
+    }
+}
