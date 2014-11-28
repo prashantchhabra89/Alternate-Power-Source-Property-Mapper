dfc4c9cfa5e973152f14704af3e2a53c61e1e479
Prashant Chhabra <prashant.chhabra89@gmail.com>
2014-11-27 21:04:56 -0800

    working screenshot without google maps

---
+++ PowerPlanner/war/Scripts/screenshotgenerator.js
@@ -0,0 +1,12 @@
+/*this script is responsible for generating screenshot of client side using html2canvas*/
+function generateImage() {
+	//generating screenshot of whole body
+   html2canvas($("#bodytag"), {
+	   //when canvas is rendered
+     onrendered: function(canvas) {
+         var myImage = canvas.toDataURL("image/png");
+         //opens image in new window
+         window.open(myImage);
+       }
+  });
+};
